cf23b9a80229fab2500eda6289adc7f004d984ad
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-26 21:31:06 +0100

    Add Task09 and solved problem: Программа выводит случайное число из отрезка (10, 99) и показывает наибольшую цифру числа

---
+++ Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа выводит случайное число из отрезка (10, 99) и показывает наибольшую цифру числа
+// 56 -- 6, 82--8, 17 -- 7.
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"random number from the segment 10-99 --> {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if(firstDigit > secondDigit) Console.WriteLine($"The greater digit of the number is  {firstDigit}");
+else Console.WriteLine($"The greater digit of the number is  {secondDigit}");
