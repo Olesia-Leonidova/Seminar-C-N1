96b9b43ff1df495a26b2a31c94fa27d007e060fe
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-26 14:02:17 +0100

    Solved Problem 4 - Task06

---
+++ Task06 Задача_4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 4
+//Принимает на вход 3 числа, найти максимум.
+
+Console.WriteLine("Enter 3 integers to see the maximal.");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+{
+max = b;
+}
+
+if(max < c)
+{
+max = c;
+}
+Console.WriteLine($"Max is {max}");
