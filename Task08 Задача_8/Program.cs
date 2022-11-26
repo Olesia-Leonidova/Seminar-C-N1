3e8fddd058c3f1e4dd77d84bf7bbd39b8db38cea
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-26 16:15:57 +0100

    Solved Problem 8 - Task08

---
+++ Task08 Задача_8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8
+//Принимает 1 число N, а на вывод от 1 до И все ЧЁТНЫЕ числа, напимер 
+// N = 9-> 2, 4, 6, 8.
+//N = 6 -> 2, 4, 6
+
+Console.WriteLine("Enter a number to see a sequence of even numbers from 1 to the entered number.");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if(N <= 1)
+{
+        Console.WriteLine("There is no diapazone of even numbers for entered value.");
+}  
+while(count <=  N)
+{
+    Console.Write($"{count} ");
+    count += 2;    
+}
+
+
+
+
+
+
+
