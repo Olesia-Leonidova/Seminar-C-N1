8d18d42f80e1cf685f8b7a632fa3f7d2ebaa0cfd
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-26 14:13:41 +0100

    Solved Problem 6 - Task07.

---
+++ Task07 Задача_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6
+// На вход принимает 1 число. Выдает, является ли оно чётным
+
+Console.WriteLine("Enter an nteger to see if it is even or odd");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N % 2 == 0)
+{
+    Console.WriteLine($"Yes, the integer {N} is even");
+}
+else
+{
+    Console.WriteLine($"No.The integer {N} is not even, it's odd.");
+}
