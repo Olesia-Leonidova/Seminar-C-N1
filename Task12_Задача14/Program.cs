7b10b299ae79616d8c2a32ae884644c9384d2f9a
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-27 20:54:36 +0100

    Add Task 13. Solved problem 16.

--- Task12_Задача14/Program.cs
+++ Task12_Задача14/Program.cs
@@ -6,3 +6,16 @@
 // 161 -> да
 
 
+//if(number > 5 && number < 10)
+
+
+Console.WriteLine("Enter a number to find out if it is multiple to 7 & 23:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity(int num)
+{
+return num % 7 == 0 && num % 23 == 0;
+}
+
+bool result = Multiplicity(numero);
+Console.WriteLine(result ? "Yes, the number is multiple to 7 & 23." : "Not multiple to 7 & 23");
---
+++ Task13 Задача_16/Program.cs
@@ -0,0 +1,35 @@
+﻿// 16. Напишите программу, которая принимает на 
+// вход два числа и проверяет, является ли одно 
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+// Так решила задачу я:
+Console.WriteLine("Enter two numbers to find out if one is a square of another.");
+Console.WriteLine("Enter 1st number:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 2nd number:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool Square(int n1, int n2)
+{
+    return n1 == n2 *n2 || n2 == n1 * n1;
+}
+bool result = Square(a, b);
+Console.WriteLine(result ? "Yes." : "No.");
+
+
+//А так решили ребята на вэбинаре:
+// Console.WriteLine("Введите первое число:");
+// int num1= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int num2= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(square(num1,num2) ? "Да" : "Нет");
+
+// bool square (int firstNum, int secondNum)
+// {
+// return (firstNum == secondNum*secondNum) || (secondNum == firstNum*firstNum);
+// }
