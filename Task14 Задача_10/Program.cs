c2eea28a5d911ae9a0d1924b2ae4894f3089caee
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-29 19:18:14 +0100

    Solved problem 13

--- Task14 Задача_10/Program.cs
+++ Task14 Задача_10/Program.cs
@@ -6,3 +6,17 @@
 // 782 -> 8 
 // 918 -> 1
 
+// идея решения: n / 10  а потом снова на 10 но через % и получем второе число
+
+// Применяем функции (методы)
+
+Console.WriteLine("Enter a 3-digit number:");
+int nmbr = Convert.ToInt32(Console.ReadLine());
+
+if (99 < nmbr < 1000)
+{
+    int digit2 = (nmbr / 10) % 10;
+    Console.WriteLine($"The 2nd digit of entered number is: {digit2} ");
+}
+
+Console.WriteLine("The entered value is not valid.");
--- Task15 Задача_13/Program.cs
+++ Task15 Задача_13/Program.cs
@@ -4,3 +4,21 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+// Применяем функции (методы)
+
+Console.WriteLine("Enter a number to see it's 3rd digit:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int ThirdDigit(int num)
+{
+    while (num > 999)
+          {
+           num =  num / 10;
+          }
+           int digit3 = num % 10; 
+           return digit3; 
+}
+ 
+
+if (a > 999) Console.WriteLine($"The 3rd digit is {ThirdDigit(a)}");
+else Console.WriteLine("There is no 3rd digit");
