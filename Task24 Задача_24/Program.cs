1962dff9cae5d840c8c3ce2374f10096d9612075
Olesia Leonidova <pozitifchick@gmail.com>
2022-12-10 21:01:49 +0100

    Solved Prooblems 19, 21 & 23

---
+++ Task24 Задача_24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Enter a number to get a result of a sum off all numbers from one up to your number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNums(int num)
+{
+    int sum = 0; //либо int sum = default
+    for (int i = 1; i <= num; i++)
+    {
+       sum = sum + i; 
+    }
+    return sum;
+}
+
+if(number > 0)
+{
+int sumNums = SumNums(number);
+}
+else Console.WriteLine("Not valid value.");
